31ba5e1a077e151b8843a9a4a8d3e540030db355
Peter Fiedler <peter.sebastian.fiedler@gmail.com>
2019-05-10 23:32:31 +0200

    #7: 'build' workflow running TypeScript compilation and unit testing tasks.

--- api-local/gulpfile.js
+++ api-local/gulpfile.js
@@ -1,13 +1,1 @@
-const {exec} = require("child_process");
-const log = require("fancy-log");
-const {series} = require("gulp");
-
-const compileTsFiles = (done) => {
-    exec("webpack --colors --mode=development", (err, stdout) => {
-        log(stdout);
-        done(err);
-    });
-};
-
-exports.compileTsFiles = series(compileTsFiles);
-exports.build = series(compileTsFiles);
+module.exports = require("gulp-workflows");
---
+++ api-local/src/files/spec/list-files-in-dir.unit.ts
@@ -0,0 +1,8 @@
+import {expect} from "chai";
+
+describe("foo", () => {
+
+    it("bar", () => {
+        expect(true).to.be.true;
+    });
+});
