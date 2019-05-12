862734202b8c2235738013d5df91220441cfe376
Peter Fiedler <peter.sebastian.fiedler@gmail.com>
2019-05-12 21:33:43 +0200

    #5: Prettify .js and .ts files script for 'archiveboy'.

--- api-local/src/files/spec/list-files-in-dir.integration.ts
+++ api-local/src/files/spec/list-files-in-dir.integration.ts
@@ -1,7 +1,6 @@
 import {expect} from "chai";
 
 describe("bar", () => {
-
     it("should be true", () => {
         expect(true).to.be.true;
     });
--- api-local/src/files/spec/list-files-in-dir.unit.ts
+++ api-local/src/files/spec/list-files-in-dir.unit.ts
@@ -1,7 +1,6 @@
 import {expect} from "chai";
 
 describe("foo", () => {
-
     it("should be true", () => {
         expect(true).to.be.true;
     });
