19e9aebc362550557cc88e4ceb0824c68b23ed26
Peter Fiedler <peter.sebastian.fiedler@gmail.com>
2019-06-21 22:27:52 +0200

    #14: Renamed the function of the 'Build GUI' task in the 'gui' module.

--- gui/gulp/tasks/build-gui.task-factory.js
+++ gui/gulp/tasks/build-gui.task-factory.js
@@ -2,7 +2,7 @@ const {exec} = require("child_process");
 const log = require("fancy-log");
 
 const createBuildGuiTask = moduleName =>
-    function compileTsFiles(done) {
+    function buildGui(done) {
         exec("ng build", (err, stdout) => {
             log(`${moduleName}: Building GUI.`);
             log(stdout);
