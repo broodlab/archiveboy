112e4fb3911122de92fba179bad127bb3a987c65
Peter Fiedler <peter.sebastian.fiedler@gmail.com>
2019-06-22 22:25:36 +0200

    #18: 'dev-unit-test' workflow for the 'gui' module.

--- gui/gulpfile.js
+++ gui/gulpfile.js
@@ -1,9 +1,10 @@
-const {createLintWorkflow} = require("gulp-tasks-and-workflows/workflows");
+const {createDevUnitTestWorkflow, createLintWorkflow} = require("gulp-tasks-and-workflows/workflows");
 const {createBuildWorkflow} = require("./gulp/workflows");
 
 const moduleName = "gui";
 
 module.exports = {
     build: createBuildWorkflow(moduleName),
+    "dev-unit-test": createDevUnitTestWorkflow(moduleName),
     lint: createLintWorkflow(moduleName)
 };
