1ed465ab168f3154a9b3651726623f3c20dd0a6b
Peter Fiedler <peter.sebastian.fiedler@gmail.com>
2019-06-24 22:40:45 +0200

    #20: 'e2e-test' workflow for the 'gui' module.

--- gui/e2e/src/app.e2e-spec.ts
+++ gui/e2e/src/app.e2e-spec.ts
@@ -1,28 +1,26 @@
-// import {AppPage} from "./app.po";
-// import {browser, logging} from "protractor";
-//
-// describe("workspace-project App", () => {
-//     let page: AppPage;
-//
-//     beforeEach(() => {
-//         page = new AppPage();
-//     });
-//
-//     it("should display welcome message", () => {
-//         page.navigateTo();
-//         expect(page.getTitleText()).toEqual("Welcome to archiveboy-gui!");
-//     });
-//
-//     afterEach(async () => {
-//         // Assert that there are no errors emitted from the browser
-//         const logs = await browser
-//             .manage()
-//             .logs()
-//             .get(logging.Type.BROWSER);
-//         expect(logs).not.toContain(
-//             jasmine.objectContaining({
-//                 level: logging.Level.SEVERE
-//             } as logging.Entry)
-//         );
-//     });
-// });
+import {AppPage} from "./app.po";
+import {browser, logging} from "protractor";
+import {expect} from "chai";
+import {beforeEach, describe, it} from "mocha";
+
+describe("workspace-project App", () => {
+    let page: AppPage;
+
+    beforeEach(() => {
+        page = new AppPage();
+    });
+
+    it("should display welcome message", async () => {
+        await page.navigateTo();
+        expect(await page.getTitleText()).to.be.eq("Welcome to ArchiveBoy!");
+    });
+
+    afterEach(async () => {
+        // Assert that there are no errors emitted from the browser
+        const logs = await browser
+            .manage()
+            .logs()
+            .get(logging.Type.BROWSER);
+        expect(logs).not.to.have.property("level", logging.Level.SEVERE);
+    });
+});
