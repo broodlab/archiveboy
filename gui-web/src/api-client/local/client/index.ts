a9d1e213eed20197c46e871927e840d0621ad1d1
Peter Fiedler <peter.sebastian.fiedler@gmail.com>
2020-10-05 13:48:09 +0200

    Fixed failing build.

--- gui-web/src/api-client/local/client/index.ts
+++ gui-web/src/api-client/local/client/index.ts
@@ -1,7 +1,3 @@
 import {Api} from "@archiveboy/api";
 
-for (let x in window["require"]("electron")) {
-    console.log(x);
-}
-
 export const api: Api = window["require"]("electron").remote.getGlobal("api");
